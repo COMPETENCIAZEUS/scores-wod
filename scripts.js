65f10904d77eb42740cb4511496040dcb3a7c89f
COMPETENCIAZEUS <enrique.heredia.bernal@gmail.com>
2024-06-16 09:30:52 -0600

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,37 +3,205 @@ document.addEventListener('DOMContentLoaded', function() {
     const categories = {
         beginner: {
             men: [
-                { name: 'Juan Pérez', position: 1, points: 0, events: 0, heats: 'Heat 1', lane: 3, schedule: '10:00 AM' },
-                { name: 'Carlos Díaz', position: 2, points: 0, events: 0, heats: 'Heat 1', lane: 4, schedule: '10:00 AM' },
-                { name: 'Enrique 4to mjr dl pais', position: 3, points: 0, events: 0, heats: 'Heat 1', lane: 4, schedule: '10:00 AM' },
+                {
+                    name: 'Juan Pérez',
+                    position: 1,
+                    points: 0,
+                    events: 0,
+                    heats: 'Heat 1',
+                    lane: 3,
+                    schedule: '10:00 AM',
+                    eventDetails: [
+                        { name: 'Evento 1', repetitions: 0, additionalInfo: '10:00 AM' },
+                        { name: 'Evento 2', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 3', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 4', repetitions: 0, additionalInfo: 'SIN INFO' }
+                    ]
+                },
+                {
+                    name: 'Carlos Díaz',
+                    position: 2,
+                    points: 0,
+                    events: 0,
+                    heats: 'Heat 1',
+                    lane: 4,
+                    schedule: '10:00 AM',
+                    eventDetails: [
+                        { name: 'Evento 1', repetitions: 0, additionalInfo: '10:00 AM' },
+                        { name: 'Evento 2', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 3', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 4', repetitions: 0, additionalInfo: 'SIN INFO' }
+                    ]
+                },
             ],
             women: [
-                { name: 'Ana López', position: 1, points: 0, events: 0, heats: 'Heat 0', lane: 2, schedule: '11:00 AM' },
-                { name: 'María Gómez', position: 2, points: 0, events: 0, heats: 'Heat 0', lane: 3, schedule: '11:00 AM' },
+                {
+                    name: 'Ana López',
+                    position: 1,
+                    points: 0,
+                    events: 0,
+                    heats: 'Heat 2',
+                    lane: 2,
+                    schedule: '11:00 AM',
+                    eventDetails: [
+                        { name: 'Evento 1', repetitions: 0, additionalInfo: '11:00 AM' },
+                        { name: 'Evento 2', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 3', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 4', repetitions: 0, additionalInfo: 'SIN INFO' }
+                    ]
+                },
+                {
+                    name: 'María Gómez',
+                    position: 2,
+                    points: 0,
+                    events: 0,
+                    heats: 'Heat 2',
+                    lane: 3,
+                    schedule: '11:00 AM',
+                    eventDetails: [
+                        { name: 'Evento 1', repetitions: 0, additionalInfo: '11:00 AM' },
+                        { name: 'Evento 2', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 3', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 4', repetitions: 0, additionalInfo: 'SIN INFO' }
+                    ]
+                },
             ]
         },
         intermediate: {
             men: [
-                { name: 'Luis Fernández', position: 0, points: 0, events: 5, heats: 'Heat 3', lane: 1, schedule: '12:00 PM' },
-                { name: 'Miguel Torres', position: 0, points: 0, events: 5, heats: 'Heat 3', lane: 2, schedule: '12:00 PM' },
+                {
+                    name: 'Luis Fernández',
+                    position: 1,
+                    points: 0,
+                    events: 0,
+                    heats: 'Heat 3',
+                    lane: 1,
+                    schedule: '12:00 PM',
+                    eventDetails: [
+                        { name: 'Evento 1', repetitions: 0, additionalInfo: '12:00 PM' },
+                        { name: 'Evento 2', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 3', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 4', repetitions: 0, additionalInfo: 'SIN INFO' }
+                    ]
+                },
+                {
+                    name: 'Miguel Torres',
+                    position: 2,
+                    points: 0,
+                    events: 0,
+                    heats: 'Heat 3',
+                    lane: 2,
+                    schedule: '12:00 PM',
+                    eventDetails: [
+                        { name: 'Evento 1', repetitions: 68, additionalInfo: '12:00 PM' },
+                        { name: 'Evento 2', repetitions: 70, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 3', repetitions: 72, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 4', repetitions: 74, additionalInfo: 'SIN INFO' }
+                    ]
+                },
             ],
             women: [
-                { name: 'Laura Ramírez', position: 0, points: 0, events: 5, heats: 'Heat 4', lane: 1, schedule: '1:00 PM' },
-                { name: 'Elena Morales', position: 0, points: 0, events: 5, heats: 'Heat 4', lane: 2, schedule: '1:00 PM' },
+                {
+                    name: 'Laura Ramírez',
+                    position: 1,
+                    points: 0,
+                    events: 0,
+                    heats: 'Heat 4',
+                    lane: 1,
+                    schedule: '1:00 PM',
+                    eventDetails: [
+                        { name: 'Evento 1', repetitions: 65, additionalInfo: '1:00 PM' },
+                        { name: 'Evento 2', repetitions: 68, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 3', repetitions: 70, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 4', repetitions: 72, additionalInfo: 'SIN INFO' }
+                    ]
+                },
+                {
+                    name: 'Elena Morales',
+                    position: 2,
+                    points: 0,
+                    events: 0,
+                    heats: 'Heat 4',
+                    lane: 2,
+                    schedule: '1:00 PM',
+                    eventDetails: [
+                        { name: 'Evento 1', repetitions: 0, additionalInfo: '1:00 PM' },
+                        { name: 'Evento 2', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 3', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 4', repetitions: 0, additionalInfo: 'SIN INFO' }
+                    ]
+                },
             ]
         },
         advanced: {
             men: [
-                { name: 'David Hernández', position: 0, points: 0, events: 5, heats: 'Heat 5', lane: 1, schedule: '2:00 PM' },
-                { name: 'Jorge Castillo', position: 0, points: 0, events: 5, heats: 'Heat 5', lane: 2, schedule: '2:00 PM' },
+                {
+                    name: 'David Hernández',
+                    position: 1,
+                    points: 0,
+                    events: 0,
+                    heats: 'Heat 5',
+                    lane: 1,
+                    schedule: '2:00 PM',
+                    eventDetails: [
+                        { name: 'Evento 1', repetitions: 0, additionalInfo: '2:00' },
+                        { name: 'Evento 2', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 3', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 4', repetitions: 0, additionalInfo: 'SIN INFO' }
+                    ]
+                },
+                {
+                    name: 'Jorge Castillo',
+                    position: 2,
+                    points: 0,
+                    events: 0,
+                    heats: 'Heat 5',
+                    lane: 2,
+                    schedule: '2:00 PM',
+                    eventDetails: [
+                        { name: 'Evento 1', repetitions: 0, additionalInfo: '2:00 PM' },
+                        { name: 'Evento 2', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 3', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 4', repetitions: 0, additionalInfo: 'SIN INFO' }
+                    ]
+                },
             ],
             women: [
-                { name: 'Lucía Martínez', position: 0, points: 0, events: 5, heats: 'Heat 6', lane: 1, schedule: '3:00 PM' },
-                { name: 'Sara Mendoza', position: 0, points: 0, events: 5, heats: 'Heat 6', lane: 2, schedule: '3:00 PM' },
+                {
+                    name: 'Lucía Martínez',
+                    position: 1,
+                    points: 0,
+                    events: 0,
+                    heats: 'Heat 6',
+                    lane: 1,
+                    schedule: '3:00 PM',
+                    eventDetails: [
+                        { name: 'Evento 1', repetitions: 0, additionalInfo: '3:00 PM' },
+                        { name: 'Evento 2', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 3', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 4', repetitions: 0, additionalInfo: 'SIN INFO' }
+                    ]
+                },
+                {
+                    name: 'Sara Mendoza',
+                    position: 2,
+                    points: 0,
+                    events: 0,
+                    heats: 'Heat 6',
+                    lane: 2,
+                    schedule: '3:00 PM',
+                    eventDetails: [
+                        { name: 'Evento 1', repetitions: 0, additionalInfo: '3:00 PM' },
+                        { name: 'Evento 2', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 3', repetitions: 0, additionalInfo: 'SIN INFO' },
+                        { name: 'Evento 4', repetitions: 0, additionalInfo: 'SIN INFO' }
+                    ]
+                },
             ]
         }
     };
 
+    // Renderizar tablas por categoría y género
     function renderCategory(category, gender, containerId) {
         const container = document.getElementById(containerId);
         const participants = categories[category][gender];
@@ -42,7 +210,7 @@ document.addEventListener('DOMContentLoaded', function() {
                             <thead>
                                 <tr>
                                     <th>Posición</th>
-                                    <th>Nombre del guerrero zeus!!</th>
+                                    <th>Nombre</th>
                                     <th>Puntaje</th>
                                 </tr>
                             </thead>
@@ -62,6 +230,7 @@ document.addEventListener('DOMContentLoaded', function() {
         container.innerHTML = tableHtml;
     }
 
+    // Renderizar todas las categorías
     function renderAllCategories() {
         renderCategory('beginner', 'men', 'beginner-men');
         renderCategory('beginner', 'women', 'beginner-women');
@@ -71,17 +240,24 @@ document.addEventListener('DOMContentLoaded', function() {
         renderCategory('advanced', 'women', 'advanced-women');
     }
 
+    // Mostrar detalles del participante en un modal
     function showParticipantDetails(category, gender, name) {
         const participant = categories[category][gender].find(p => p.name === name);
         const modalBody = document.querySelector('.modal-body');
         
-        let detailsHtml = `<h6>Nombre del guerrero zeus!!:</h6> <p>${participant.name}</p>
+        let detailsHtml = `<h6>Nombre:</h6> <p>${participant.name}</p>
                            <h6>Puntaje:</h6> <p>${participant.points}</p>
                            <h6>Eventos Realizados:</h6> <p>${participant.events}</p>
                            <h6>Heat:</h6> <p>${participant.heats}</p>
                            <h6>Carril:</h6> <p>${participant.lane}</p>
-                           <h6>Horario:</h6> <p>${participant.schedule}</p>`;
+                           <h6>Horario de proximo evento:</h6> <p>${participant.schedule}</p>`;
         
+        participant.eventDetails.forEach((event, index) => {
+            detailsHtml += `<h6>${event.name}:</h6>
+                            <p>Repeticiones: ${event.repetitions}</p>
+                            <p>Información Adicional: ${event.additionalInfo}</p>`;
+        });
+
         modalBody.innerHTML = detailsHtml;
         $('#participantModal').modal('show');
     }
